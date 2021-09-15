1f1579a3a42be1ecb805549a1794a200289f7bba
erkutbas <ebas1@binghamton.edu>
2021-09-15 05:12:51 +0300

    Bug Fixed

--- Sources/NetworkLayer/Modules/ApiInterceptor.swift
+++ Sources/NetworkLayer/Modules/ApiInterceptor.swift
@@ -12,8 +12,6 @@ import Alamofire
 /// Description: Interceptor layer is responsible to combine adapter and retrier modules
 final public class ApiInterceptor: Interceptor {
 
-    public init() {}
-    
     public override init(adapter: RequestAdapter, retrier: RequestRetrier) {
         super.init(adapter: adapter, retrier: retrier)
     }
